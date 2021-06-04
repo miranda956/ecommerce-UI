2a8bbe84cd26d14060e06b0107122a23f8c98c24
miranda956 <musularonald17@gmail.com>
2021-06-04 10:50:59 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-    Welcome Site Under Development 
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> 
+          Welcome Site Under Development
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
